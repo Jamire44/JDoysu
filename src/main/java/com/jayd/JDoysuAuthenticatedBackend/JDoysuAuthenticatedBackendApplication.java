0ff03927dfde3fd5d6320f4e308612c1f606c260
Jamire44 <2003doyle@gmail.com>
2024-02-16 17:35:16 +0000

    initial commit

---
+++ src/main/java/com/jayd/JDoysuAuthenticatedBackend/JDoysuAuthenticatedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jayd.JDoysuAuthenticatedBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JDoysuAuthenticatedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JDoysuAuthenticatedBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jayd/JDoysuAuthenticatedBackend/JDoysuAuthenticatedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayd.JDoysuAuthenticatedBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JDoysuAuthenticatedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
